4a143f298e77566842a62340e48ae85362b2e978
Emma Chan <emma.chan25@ycdsbk12.ca>
2024-03-21 13:43:39 -0400

    finish

--- Sketch.java
+++ Sketch.java
@@ -16,21 +16,62 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // grid quadrent 
+    for (int lineX = 20; lineX < 200; lineX += 20) {
+      stroke (0,0,0); 
+      line(lineX, 0, lineX, 200); 
+    } 
+    for (int lineY = 20; lineY <= 200; lineY += 20) {
+      stroke (0,0,0); 
+      line(0, lineY, 200, lineY); 
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Circle quadrent 
+    for (int circleY = 30; circleY <=200; circleY += 35) {
+      for (int circleX = 230; circleX <= 400; circleX += 35){
+        fill (212, 15, 125); 
+        stroke (0,0,0); 
+        ellipse (circleX, circleY, 20,20); 
+      }
+    }
+
+    // Gradient quadrent 
+      double colour = 0;  
+      for (int lineX = 0; lineX <= 200; lineX ++){
+        line (lineX, 200, lineX, 400);
+  
+        colour += 1.25; 
+        stroke ((int)colour, (int)colour, (int)colour); 
+      }
+
+    //Flower Quadrent 
+      //draw petals 
+      
+      for(int i = 0; i < 2; i++){
+        rotate(radians(45*i));
+        fill(219, 84, 37);
+        stroke(0, 0, 0);
+        ellipse(424*sin(radians(45+45*i)), 424*cos(radians(45+45*i)), 125, 18);
+
+        fill(219, 84, 37);
+        stroke(0, 0, 0);
+        ellipse(424*sin(radians(45+45*i)), 424*cos(radians(45+45*i)), 18, 125);  
+      }
+
+      // Reset the coordiante system
+      rotate(radians(315));
+
+      //circle 
+      fill(37, 108, 45); 
+      ellipse (300,300,32,32); 
+    } 
   }
   
-  // define other methods down here.
-}
+  // define other methods down here
